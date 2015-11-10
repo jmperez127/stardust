a5dfbdfe8bf95c7d3ff06270e212db5df6b7a9a3
theil <theil_27@hotmail.com>
2015-11-09 22:33:21 -0430

    Add spec helpers

---
+++ spec/config/ApplicationSpec.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace spec;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ApplicationSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Application');
+    }
+
+    function it_can_be_initialized_with_an_env_variable()
+    {
+        $app = $this->setup("env");
+        $this->equalTo($app->enviroment, "env", "env");
+    }
+}
---
+++ spec/spec_helpers.php
@@ -0,0 +1,4 @@
+<?php
+
+if (!defined('PROJECT_ROOT'))
+    define('PROJECT_ROOT', realpath(__DIR__ . '/..'));
