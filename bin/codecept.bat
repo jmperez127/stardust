a16ae61b4932991c04d0a26b19239dacf0bf96a8
theil <theil_27@hotmail.com>
2015-11-13 21:01:13 -0430

    Travis script adjustments.

--- bin/codecept.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-SET BIN_TARGET=%~dp0/../vendor/composer/codeception/codeception/codecept
-php "%BIN_TARGET%" %*
--- bin/phpspec.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-SET BIN_TARGET=%~dp0/../vendor/composer/phpspec/phpspec/bin/phpspec
-php "%BIN_TARGET%" %*
